197f501c8101c38ccf1193b6d693080414f95c1c
John Mburu <johnnkonge2020@gmail.com>
2025-10-12 10:01:36 +0300

    continue in c++

---
+++ continue.cpp
@@ -0,0 +1,12 @@
+////using continue statement to skip an iteration
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int n = 10; n>0 ; n--){
+        if(n == 5) continue;
+        cout << n << ",";
+    }
+    cout << "Liftoff!!!";
+}
